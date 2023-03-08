ab1e6145f36401575d8b5e86f9e17528122105ca
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2023-03-08 03:13:17 +0200

    Initial Commit

---
+++ Ahmed's Card/Ahmed_s_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ahmed_s_CardApp.swift
+//  Ahmed's Card
+//
+//  Created by AHMED GAMAL  on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Ahmed_s_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
